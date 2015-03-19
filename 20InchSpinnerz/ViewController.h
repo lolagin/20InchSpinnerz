ea4d8f06f4bd77e8a0581a576d690e8418a1e454
Jeffrey Rosenthal <jeffrey.rosenthal@gmail.com>
2015-03-19 11:06:07 -0700

    ok this time workz plz lul

---
+++ 20InchSpinnerz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20InchSpinnerz
+//
+//  Created by Jeffrey C Rosenthal on 3/19/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20InchSpinnerz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20InchSpinnerz
+//
+//  Created by Jeffrey C Rosenthal on 3/19/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
